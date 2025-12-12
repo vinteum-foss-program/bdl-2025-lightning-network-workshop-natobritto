687083765c9faf09f2fc042960ab8008f0aa5f66
Renato Britto <renatobritto.professional@gmail.com>
2025-12-12 11:18:36 +0100

    add replit link and trigger CI completion signal

--- submissions/done.sh
+++ submissions/done.sh
@@ -1,6 +1,6 @@
 # When you finish the workshop:
 # 1. add a link to your replit fork in the next line
-#
+# https://replit.com/@renatobrittopro/Programming-Lightning-Intro-to-Payment-Channels-mini?v=1
 #
 # 2. change the FAIL string to PASS.
-echo "FAIL"
+echo "PASS"
